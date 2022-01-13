0789bd3170023b743175106da7ef3f1e0c2f18ef
Shifteon <bpwyatt04@gmail.com>
2022-01-13 10:48:55 -0700

    ta02 stretch 1

--- routes/ta02.js
+++ routes/ta02.js
@@ -4,19 +4,30 @@ const express = require('express');
 const router = express.Router();
 
 const users = [];
+var message = "";
 
 router.post('/addUser', (req, res, next) => {
+  
   users.push({ name: req.body.name });
+  if (message != "") {
+    message = "";
+  }
   res.redirect('/ta02/');
 });
 
 router.post('/removeUser', (req, res, next) => {
-  console.log(users.size);
-  for (let i = 0; i < users.size; i++) {
-    console.log(users[i].name);
+  let found = false;
+  for (let i = 0; i < users.length; i++) {
     if (users[i].name == req.body.name) {
-      users.splice(i);
+      users.splice(i, 1);
+      found = true;
+      if (message != "") {
+        message = "";
+      }
     }
+  }
+  if (!found) {
+    message = "The user doesn't exist!";
   }
   res.redirect('/ta02/');
 });
@@ -27,7 +38,8 @@ router.get('/', (req, res, next) => {
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
-    users: users
+    users: users,
+    message: message
   });
 });
 
