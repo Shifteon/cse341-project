d3c8eb2b1e5a2598e275bc1c80d649e666efbe2c
Shifteon <bpwyatt04@gmail.com>
2022-01-13 11:07:43 -0700

    ta02 stretch

--- routes/ta02.js
+++ routes/ta02.js
@@ -4,14 +4,27 @@ const express = require('express');
 const router = express.Router();
 
 const users = [];
-var message = "";
+
+const params = {
+title: 'Team Activity 02',
+path: '/ta02', // For pug, EJS
+activeTA03: true, // For HBS
+contentCSS: true, // For HBS
+users: users,
+message: "",
+exists: false
+};
 
 router.post('/addUser', (req, res, next) => {
-  
-  users.push({ name: req.body.name });
-  if (message != "") {
-    message = "";
+  let exists = false;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].name == req.body.name) {
+      exists = true;
+      params.exists = true;
+    }
   }
+  if (!exists)
+    users.push({ name: req.body.name });
   res.redirect('/ta02/');
 });
 
@@ -21,26 +34,18 @@ router.post('/removeUser', (req, res, next) => {
     if (users[i].name == req.body.name) {
       users.splice(i, 1);
       found = true;
-      if (message != "") {
-        message = "";
-      }
     }
   }
   if (!found) {
-    message = "The user doesn't exist!";
+    params.message = "The user doesn't exist!";
   }
   res.redirect('/ta02/');
 });
 
 router.get('/', (req, res, next) => {
-  res.render('pages/ta02', {
-    title: 'Team Activity 02',
-    path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
-    users: users,
-    message: message
-  });
+  res.render('pages/ta02', params);
+  params.exists = false;
+  params.message = "";
 });
 
 module.exports = router;
